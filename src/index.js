5a7c4ca3c8e9136e52de789a7cca8768190540bd
mac <mac@macs-MacBook-Air.local>
2021-03-15 01:12:36 +0400

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+
+  return matrix.map((array, idx) => {
+      if (idx % 2) return array.reverse();
+      else return array;
+  }).flat();
 }
